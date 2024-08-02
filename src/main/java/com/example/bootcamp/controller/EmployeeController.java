c69dca7d0584083c94ad68bab2f447b0cc5c344b
Anton Vasilevsky <q3antonio@gmail.com>
2024-08-02 12:35:04 +0300

    dto validation works

--- src/main/java/com/example/bootcamp/controller/EmployeeController.java
+++ src/main/java/com/example/bootcamp/controller/EmployeeController.java
@@ -4,16 +4,21 @@ import com.example.bootcamp.dto.EmployeeDto;
 import com.example.bootcamp.model.Employee;
 import com.example.bootcamp.service.EmployeeService;
 import jakarta.validation.Valid;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
+@Log4j2
 public class EmployeeController {
     private final EmployeeService employeeService;
 
@@ -25,9 +30,35 @@ public class EmployeeController {
     }
 
     @PostMapping("/add")
-    public HttpStatus addEmployee(@RequestBody @Valid EmployeeDto dto) {
-        return HttpStatus.ACCEPTED;
+    public ResponseEntity<String> addEmployee(@RequestBody @Valid EmployeeDto dto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+           // getReadableStringErrors(allErrors);
+
+            log.info(allErrors.toString());
+            return ResponseEntity.badRequest().body(allErrors.toString());
+        }
+        log.info(dto);
+        Employee employee = convertOneToEntity(dto);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
+
+    private String getReadableStringErrors(List<ObjectError> allErrors) {
+        // todo mb finish later
+        ObjectError objectError = allErrors.get(0);
+        String errorMessage = objectError.getDefaultMessage();
+
+        Object[] arguments = objectError.getArguments();
+        DefaultMessageSourceResolvable argument = arguments[0] instanceof DefaultMessageSourceResolvable ?  (DefaultMessageSourceResolvable) arguments[0] : null;
+        String defaultMessage = argument.getDefaultMessage();
+
+        return defaultMessage + ":" + errorMessage;
+    }
+
+    private Employee convertOneToEntity(EmployeeDto dto) {
+        return modelMapper.map(dto, Employee.class);
+    }
+
     @GetMapping("/all")
     public List<EmployeeDto>getAll() {
         List<Employee> all = employeeService.findAll();
